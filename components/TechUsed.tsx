08da4709582ae1963ce2f85333d08533ca61c7ad
Romulo Negrillo Jr. <romnegrillo@gmail.com>
2024-07-24 21:33:20 +0800

    Initial setup of default default styles and Aceternity.

---
+++ components/TechUsed.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TechUsed = () => {
+  return <div>Tech Used</div>;
+};
+
+export default TechUsed;
---
+++ components/index.ts
@@ -0,0 +1,21 @@
+import NavBar from "./NavBar";
+import Services from "./Services";
+import Process from "./Process";
+import Projects from "./Projects";
+import Testimonials from "./Testimonials";
+import Team from "./Team";
+import TechUsed from "./TechUsed";
+import Contact from "./Contact";
+import Footer from "./Footer";
+
+export {
+  NavBar,
+  Services,
+  Process,
+  Projects,
+  Testimonials,
+  Team,
+  TechUsed,
+  Contact,
+  Footer,
+};
