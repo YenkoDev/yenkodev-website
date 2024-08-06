a7c2759006b48d9d811901fe230716ebf8b4e4d9
frosteen <luispambid@gmail.com>
2024-08-06 19:36:07 +0800

    Added in progress for team and testimonials

--- app/team/content.tsx
+++ app/team/content.tsx
@@ -16,6 +16,17 @@ const Content = () => {
           <h1 className="text-3xl font-bold w-full">Team</h1>
         </div>
       </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.8 }}
+        transition={{ duration: 0.5 }}
+      >
+        <div className="flex justify-center mt-8">
+          <h2 className="text-2xl w-full text-red-500">In progress...</h2>
+        </div>
+      </motion.div>
     </main>
   );
 };
