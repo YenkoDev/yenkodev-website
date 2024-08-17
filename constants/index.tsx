6a531765809d05bce7e011fbc1928eddff7fc6e2
frosteen <luispambid@gmail.com>
2024-08-17 19:11:58 +0800

    Commented out for now, for formality.

--- constants/index.tsx
+++ constants/index.tsx
@@ -41,8 +41,8 @@ export const navLinks = [
   { title: "Services", href: "/services" },
   { title: "Process", href: "/process" },
   { title: "Projects", href: "/projects" },
-  { title: "Testimonials", href: "/testimonials" },
-  { title: "Team", href: "/team" },
+  // { title: "Testimonials", href: "/testimonials" },
+  // { title: "Team", href: "/team" },
 ];
 
 // Homepage contents.
